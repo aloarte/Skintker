0b79d120606638f4b110770c1fed8021e4606f74
Albrath <aloarter@gmail.com>
2022-01-02 22:48:46 +0100

    Unit test for some DataParser methods

--- app/src/test/java/com/p4r4d0x/skintker/KoinBaseTest.kt
+++ app/src/test/java/com/p4r4d0x/skintker/KoinBaseTest.kt
@@ -1,22 +1,22 @@
 package com.p4r4d0x.skintker
 
-import androidx.test.core.app.ApplicationProvider
-import org.junit.After
-import org.junit.Before
-import org.koin.core.module.Module
-import org.koin.test.KoinTest
-
-open class KoinBaseTest(private vararg val modules: Module = emptyArray()) : KoinTest {
-    private lateinit var app: KoinTestApplication
-
-    @Before
-    fun init() {
-        app = ApplicationProvider.getApplicationContext() as KoinTestApplication
-        app.injectModule(modules)
-    }
-
-    @After
-    fun finish() {
-        app.unloadModules(modules)
-    }
-}
+//import androidx.test.core.app.ApplicationProvider
+//import org.junit.After
+//import org.junit.Before
+//import org.koin.core.module.Module
+//import org.koin.test.KoinTest
+//
+//open class KoinBaseTest(private vararg val modules: Module = emptyArray()) : KoinTest {
+//    private lateinit var app: KoinTestApplication
+//
+//    @Before
+//    fun init() {
+//        app = ApplicationProvider.getApplicationContext() as KoinTestApplication
+//        app.injectModule(modules)
+//    }
+//
+//    @After
+//    fun finish() {
+//        app.unloadModules(modules)
+//    }
+//}
