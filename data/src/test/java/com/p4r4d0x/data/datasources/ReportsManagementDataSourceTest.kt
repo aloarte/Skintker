8614e43846df321e0664cc674302178938fc45ef
Albrath <aloarter@gmail.com>
2023-03-12 21:36:29 +0100

    Finished datasource tests

--- data/src/test/java/com/p4r4d0x/data/datasources/ReportsManagementDataSourceTest.kt
+++ data/src/test/java/com/p4r4d0x/data/datasources/ReportsManagementDataSourceTest.kt
@@ -35,6 +35,7 @@ class ReportsManagementDataSourceTest :
 
     companion object {
         const val USER_ID = "userId"
+        const val REPORT_DATE = "09-02-2023"
         const val OFFSET = 0
         const val LIMIT = 4
         const val ecInvalidDate = -2
@@ -97,7 +98,6 @@ class ReportsManagementDataSourceTest :
                 "        \"count\": 1\n" +
                 "    }\n" +
                 "}"
-
         private const val JSON_LOG_EMPTY_LIST = "{\n" +
                 "    \"statusCode\": 0,\n" +
                 "    \"content\": {\n" +
@@ -106,7 +106,14 @@ class ReportsManagementDataSourceTest :
                 "        \"count\": 0\n" +
                 "    }\n" +
                 "}"
-
+        private const val JSON_DELETE_LOG = " {\n" +
+                "            \"statusCode\": 0,\n" +
+                "            \"statusMessage\": \"Report removed correctly\"\n" +
+                "}"
+        private const val JSON_DELETE_LOGS = " {\n" +
+                "            \"statusCode\": 0,\n" +
+                "            \"statusMessage\": \"Reports removed correctly\"\n" +
+                " }"
     }
 
     private val logBo = DailyLogBO(
@@ -247,13 +254,8 @@ class ReportsManagementDataSourceTest :
 
     @Test
     fun `test get report list paginated error`() {
-        coEvery {
-            api.getReportsPaginated(
-                USER_ID,
-                LIMIT,
-                OFFSET
-            )
-        } returns buildResponse(resultCode = 404)
+        coEvery { api.getReportsPaginated(USER_ID, LIMIT, OFFSET) } returns
+                buildResponse(resultCode = 404)
 
         val logList = runBlocking { datasource.getReports(USER_ID, LIMIT, OFFSET) }
 
@@ -262,100 +264,27 @@ class ReportsManagementDataSourceTest :
         Assertions.assertEquals(expected, logList)
     }
 
-//    @Test
-//    fun `test get report list success`() {
-//        val logContents = DailyLogContentsBO(count = 1, logList = logList)
-//        coEvery { datasource.getReports(USER_ID) } returns ApiResult.Success(logContents)
-//
-//        val logInserted = runBlocking { repository.getReports(USER_ID) }
-//
-//        coVerify { datasource.getReports(USER_ID) }
-//        Assertions.assertEquals(logContents, logInserted)
-//    }
-//
-//    @Test
-//    fun `test get report list error`() {
-//        coEvery { datasource.getReports(USER_ID) } returns ApiResult.Error(
-//            -1,
-//            "Error retrieving log contents"
-//        )
-//
-//        val logInserted = runBlocking { repository.getReports(USER_ID) }
-//
-//        coVerify { datasource.getReports(USER_ID) }
-//        Assertions.assertEquals(DailyLogContentsBO(), logInserted)
-//    }
-//
-//    @Test
-//    fun `test get report list paginated success`() {
-//        val logContents = DailyLogContentsBO(count = 4, logList = logList)
-//        coEvery { datasource.getReports(USER_ID, LIMIT, OFFSET) } returns ApiResult.Success(
-//            logContents
-//        )
-//
-//        val logInserted = runBlocking { repository.getReports(USER_ID, LIMIT, OFFSET) }
-//
-//        coVerify { datasource.getReports(USER_ID, LIMIT, OFFSET) }
-//        Assertions.assertEquals(logContents, logInserted)
-//    }
-//
-//    @Test
-//    fun `test get report list paginated error`() {
-//        coEvery { datasource.getReports(USER_ID, LIMIT, OFFSET) } returns ApiResult.Error(
-//            -1,
-//            "Error retrieving log contents"
-//        )
-//
-//        val logInserted = runBlocking { repository.getReports(USER_ID, LIMIT, OFFSET) }
-//
-//        coVerify { datasource.getReports(USER_ID, LIMIT, OFFSET) }
-//        Assertions.assertEquals(DailyLogContentsBO(), logInserted)
-//    }
-//
-//    @Test
-//    fun `test delete report success`() {
-//        coEvery { datasource.deleteReport(USER_ID, date.toString()) } returns
-//                ApiResult.Success(true)
-//
-//        val logDeleted = runBlocking { repository.deleteReport(USER_ID, date.toString()) }
-//
-//        coVerify { datasource.deleteReport(USER_ID, date.toString()) }
-//        Assertions.assertTrue(logDeleted)
-//    }
-//
-//    @Test
-//    fun `test delete report error`() {
-//        coEvery { datasource.deleteReport(USER_ID, date.toString()) } returns
-//                ApiResult.Error(-1, "Error not deleted")
-//
-//        val logDeleted = runBlocking { repository.deleteReport(USER_ID, date.toString()) }
-//
-//        coVerify { datasource.deleteReport(USER_ID, date.toString()) }
-//        Assertions.assertFalse(logDeleted)
-//    }
-//
-//    @Test
-//    fun `test delete reports success`() {
-//        coEvery { datasource.deleteReports(USER_ID) } returns
-//                ApiResult.Success(true)
-//
-//        val logDeleted = runBlocking { repository.deleteReports(USER_ID) }
-//
-//        coVerify { datasource.deleteReports(USER_ID) }
-//        Assertions.assertTrue(logDeleted)
-//    }
-//
-//    @Test
-//    fun `test delete reports error`() {
-//        coEvery { datasource.deleteReports(USER_ID) } returns
-//                ApiResult.Error(-1, "Error not deleted")
-//
-//        val logDeleted = runBlocking { repository.deleteReports(USER_ID) }
-//
-//        coVerify { datasource.deleteReports(USER_ID) }
-//        Assertions.assertFalse(logDeleted)
-//    }
+    @Test
+    fun `test delete report success`() {
+        coEvery { api.deleteReport(USER_ID, REPORT_DATE) } returns
+                buildResponse(resultCode = 200, json = JSON_DELETE_LOG)
 
+        val logList = runBlocking { datasource.deleteReport(USER_ID, REPORT_DATE) }
+
+        coVerify { api.deleteReport(USER_ID, REPORT_DATE) }
+        Assertions.assertEquals(ApiResult.Success(true), logList)
+    }
+
+    @Test
+    fun `test delete reports success`() {
+        coEvery { api.deleteAllReports(USER_ID) } returns
+                buildResponse(resultCode = 200, json = JSON_DELETE_LOGS)
+
+        val logList = runBlocking { datasource.deleteReports(USER_ID) }
+
+        coVerify { api.deleteAllReports(USER_ID) }
+        Assertions.assertEquals(ApiResult.Success(true), logList)
+    }
 
     private fun buildResponse(
         resultCode: Int,
